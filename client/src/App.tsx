8baab07e54e944e291a5e7fe74cfe173d4ef4d00
Bram <Bram.mathijssen@gmail.com>
2023-12-21 00:44:09 +0100

    feat: added CORS and fetch products in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async() => {
+      const res = await fetch('http://localhost:5245/api/products');
+      const data = await res.json();
+      setProducts(data)
+    }
+    fetchProducts();
+  }, [])
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+          {products.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
