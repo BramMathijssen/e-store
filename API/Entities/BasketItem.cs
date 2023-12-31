4d022b49f5d946d2cd9c6e65073c3b1bac40b294
Bram <Bram.mathijssen@gmail.com>
2023-12-31 12:14:33 +0100

    feat: added basket and basketitem entities

---
+++ API/Entities/BasketItem.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace API.Entities
+{
+    [Table("BasketItems")]
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+
+        // navigation properties
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
+    }
+}
