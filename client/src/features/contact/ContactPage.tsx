69718551edaf1308bdda2106dbb42c745aef846b
Bram <Bram.mathijssen@gmail.com>
2024-01-01 18:17:36 +0100

    feat: added basic redux boilerplate

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Button, ButtonGroup, Typography } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { CounterState, decrement, increment } from './counterReducer';
 
 const ContactPage = () => {
-  return (
-    <div>ContactPage</div>
-  )
+    const dispatch = useDispatch();
+    const {data, title} = useSelector((state: CounterState) => state);
+    
+    return (
+        <>
+        <div>Yooooo</div>
+            <Typography gutterBottom variant='h3'>{title}</Typography>
+            <Typography variant='h4'>The data is: {data}</Typography>
+            <ButtonGroup>
+                <Button onClick={() => dispatch(decrement(1))} variant='contained' color='error'>Decrement</Button>
+                <Button onClick={() => dispatch(increment(1))} variant='contained' color='primary'>Increment</Button>
+                <Button onClick={() => dispatch(increment(5))} variant='contained' color='secondary'>Increment by 5</Button>
+            </ButtonGroup>
+        </>
+
+    )
 }
 
-export default ContactPage
+export default ContactPage;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,9 +7,16 @@ import "@fontsource/roboto/700.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./app/router/Routes.tsx";
 import { StoreProvider } from "./app/context/StoreContext.tsx";
+import { Provider } from "react-redux";
+import { configureStore } from "./app/store/configureStore.ts";
+
+const store = configureStore();
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StoreProvider>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StoreProvider>
 );
