3bce180268a1d53358d62d4467e4df6931df91ab
Bram <Bram.mathijssen@gmail.com>
2024-01-15 17:36:08 +0100

    feat: added server-side sorting,filtering and pagination

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,7 +1,11 @@
+using System.Text.Json;
 using API.Data;
 using API.Entities;
+using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 
 namespace API.Controllers
 {
@@ -13,10 +17,43 @@ namespace API.Controllers
             _context = context;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        // NOTE: This is just a testing method -> Not used for production
+        [HttpGet("ProductFilter")]
+        public async Task<ActionResult<List<Product>>> GetProductFilter()
         {
-            return await _context.Products.ToListAsync();
+            // methode 1
+            IQueryable<Product> queryVanilla =
+                from p in _context.Products
+                where p.Price > 2000
+                select p;
+
+            foreach (var p in queryVanilla)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            // methode 2
+            var query = _context.Products.AsQueryable();
+            IQueryable<Product> query2 = _context.Products.AsQueryable();
+
+            return query.Where(p => p.Price >= 25000).ToList();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery] ProductParams productParams)
+        {
+            var query = _context.Products
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchTerm)
+                .Filter(productParams.Brands, productParams.Types)
+                .AsQueryable();
+
+            var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+
+            Response.Headers.Append("Pagination", JsonSerializer.Serialize(products.MetaData));
+            Response.Headers.Add("Wadup", "My Name is Tony");
+
+            return products;
         }
 
         [HttpGet("{id}")]
@@ -24,7 +61,7 @@ namespace API.Controllers
         {
             var product = await _context.Products.FindAsync(id);
 
-            if(product == null) return NotFound();
+            if (product == null) return NotFound();
 
             return product;
         }
---
+++ API/Extensions/ProductExtensions.cs
@@ -0,0 +1,48 @@
+using System.Collections.Immutable;
+using System.Diagnostics;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ProductExtensions
+{
+    public static IQueryable<Product> Sort(this IQueryable<Product> query, string orderBy)
+    {
+        if (string.IsNullOrWhiteSpace(orderBy)) return query.OrderBy(p => p.Name);
+
+        query = orderBy switch
+        {
+            "price" => query.OrderBy(p => p.Price),
+            "priceDesc" => query.OrderByDescending(p => p.Price),
+            _ => query.OrderBy(p => p.Name)
+        };
+
+        return query;
+    }
+
+    public static IQueryable<Product> Search(this IQueryable<Product> query, string searchString)
+    {
+        if (string.IsNullOrWhiteSpace(searchString)) return query;
+
+        var lowerCaseSearch = searchString.Trim().ToLower();
+
+        return query.Where(p => p.Name.ToLower().Contains(lowerCaseSearch));
+    }
+
+    public static IQueryable<Product> Filter(this IQueryable<Product> query, string brands, string types)
+    {
+        var brandList = new List<string>();
+        var typeList = new List<string>();
+
+        if (!string.IsNullOrEmpty(brands))
+            brandList.AddRange(brands.ToLower().Split(",").ToList());
+        if (!string.IsNullOrEmpty(types))
+            typeList.AddRange(types.ToLower().Split(",").ToList());
+
+        query = query.Where(p => brandList.Count == 0 || brandList.Contains(p.Brand.ToLower()));
+        query = query.Where(p => typeList.Count == 0 || typeList.Contains(p.Type.ToLower()));
+
+        return query;
+    }
+}
