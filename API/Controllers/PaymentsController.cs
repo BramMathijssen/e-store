5b0743d92007cecce35ac62432e0cc63cb2e404e
Bram <Bram.mathijssen@gmail.com>
2024-01-23 19:40:47 +0100

    chore: removed unused function from paymentcontroller

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -47,25 +47,4 @@ public class PaymentsController : BaseApiController
 
         return basket.MapBasketToDto();
     }
-
-    [AllowAnonymous]
-    [HttpPost("webhook")]
-    public async Task<ActionResult> StripeWebhook()
-    {
-        var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
-
-        var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"],
-            _config["StripeSettings:WhSecret"]);
-
-        var charge = (Charge)stripeEvent.Data.Object;
-
-        var order = await _context.Orders.FirstOrDefaultAsync(x => 
-            x.PaymentIntentId == charge.PaymentIntentId);
-
-        if (charge.Status == "succeeded") order.OrderStatus = OrderStatus.PaymentReceived;
-
-        await _context.SaveChangesAsync();
-
-        return new EmptyResult();
-    }
 }
