02de0d30aef051c90bd5c18aa75b489a45d4d051
Bram <Bram.mathijssen@gmail.com>
2024-01-24 19:29:05 +0100

    fix: changed a typo on the homepage

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from "@mui/material";
 export default function HomePage() {
     return (
         <Box display="flex" justifyContent="center" sx={{ p: 4 }}>
-            <Typography variant="h1">Welcome to the store</Typography>
+            <Typography variant="h1">Welcome to the E-Store</Typography>
         </Box>
     );
 }
